b660efe33bc0773b60426b4199226403e6d804eb
zainamroti <sayedzainjeelani@gmail.com>
2022-06-05 19:59:38 +0500

    fix sign replay vulnerability in the contract

--- contracts/MetaTokenSender.sol
+++ contracts/MetaTokenSender.sol
@@ -16,19 +16,30 @@ contract TokenSender {
 
     using ECDSA for bytes32;
 
-    function transfer(address sender, uint amount, address recipient, address tokenContract, bytes memory signature) public {
-        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract);
+    // New mapping
+    mapping(bytes32 => bool) executed;
+
+    // Add the nonce parameter here
+    function transfer(address sender, uint amount, address recipient, address tokenContract, uint nonce, bytes memory signature) public {
+        // Pass ahead the nonce
+        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract, nonce);
         bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
+
+        // Require that this signature hasn't already been executed
+        require(!executed[signedMessageHash], "Already executed!");
 
         address signer = signedMessageHash.recover(signature);
 
         require(signer == sender, "Signature does not come from sender");
 
+        // Mark this signature as having been executed now
+        executed[signedMessageHash] = true;
         bool sent = ERC20(tokenContract).transferFrom(sender, recipient, amount);
         require(sent, "Transfer failed");
     }
 
-    function getHash(address sender, uint amount, address recipient, address tokenContract) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract));
+    // Add the nonce parameter here
+    function getHash(address sender, uint amount, address recipient, address tokenContract, uint nonce) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract, nonce));
     }
 }
